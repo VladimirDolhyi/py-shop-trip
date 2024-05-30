d28fe0a9cad051aeb1f15f6369f2e90fd8d74149
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-05-30 11:44:46 +0300

    Solution+

--- app/customer.py
+++ app/customer.py
@@ -16,11 +16,9 @@ class Customer:
                             shop_location: list[int],
                             car: "Car"
                             ) -> float:
-        print("Customer location:", self.location)
-        print("Shop location:", shop_location)
+
         distance = round(math.sqrt(
-            (self.location[0] - shop_location[0]) ** 2
-            + (self.location[1] - shop_location[1]) ** 2
+            (int(self.location[0]) - int(shop_location[0])) ** 2
+            + (int(self.location[1]) - int(shop_location[1])) ** 2
         ), 2)
-        print("Distance to the shop:", distance)
         return round(distance * car.fuel_consumption * 2 * 2.4 / 100, 2)
